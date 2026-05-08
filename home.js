90a1b2584aa291fc9cfdbcc7ffe49a4fe1e809ca
alexmartoos4-rgb <alexmartoos4@gmail.com>
2026-05-08 18:29:55 +0000

    First Commit

---
+++ home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
+
+const cards = [
+	{
+		image: "https://picsum.photos/id/10/200/300",
+		title: "Parque",
+		description: "Parque con lindo paisaje",
+	},
+	{
+		image: "https://picsum.photos/id/17/200/300",
+		title: "Sendero",
+		description: "Sendero con lindo paisaje",
